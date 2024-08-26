38b2b600b982d1da1824cd3ef61f2355c883c326
Nidhipatil29 <162793454+Nidhipatil29@users.noreply.github.com>
2024-08-26 14:49:27 +0530

    Update1 app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def mainpage():
 	return "Hello from my python server"
+	return "This is jenkins pipeline"
 
 if(__name__=='__main__'):
 	app.run(host='0.0.0.0',port=5000,debug=True)
